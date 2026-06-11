c555da35586b815a5ed3c541d7ec8958c3c64d04
deepika-nagotra <deepikanagotra2006@gmail.com>
2026-06-11 15:08:08 +0530

    main

--- styles.js
+++ styles.js
@@ -1,9 +1,22 @@
-<script>
+
     let count=0;
      const cartCountSpan=document.getElementById('cart-count');
      function addToCart(){
-        count=count+1;
+        count= count + 1;
+        if(cartCountSpan){ 
         cartCountSpan.innerText=count;
+        }
         document.getElementById('cart-count').addEventListener('click',addToCart);
      }
-</script>
+     function toggleCart(){
+        const sideCart =document.getElementById('side-count');
+        if(sidecart){ 
+        sidecart.classList.toggle('open');
+        }
+     }
+     function toggleSidebar(){
+        const sideMenu =document.getElementById('side-menu');
+        if(sideMenu){ 
+        sideMenu.classList.toggle('open');
+        }
+     }
